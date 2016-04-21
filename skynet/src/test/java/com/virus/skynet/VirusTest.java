df34a7e5d3cf343ab399744427d3517f3e9abf7c
mohamedou <mohamedou.ab@gmail.com>
2016-04-21 15:21:20 +0200

    enrichissement  de tests unitaires de la methode findAndRemoveNextLink

--- skynet/src/test/java/com/virus/skynet/VirusTest.java
+++ skynet/src/test/java/com/virus/skynet/VirusTest.java
@@ -27,4 +27,35 @@ public class VirusTest {
 		assertEquals(expectedLink, nextLink);
 	}
 
+	@Test
+	public void should_find_first_link_when_agent_on_an_arbitrary_node() {
+		// arrange
+		Link expectedLink = new Link(network.getNodeByIndex(0), network.getNodeByIndex(1));
+		Link secondLink = new Link(network.getNodeByIndex(0), network.getNodeByIndex(2));
+
+		network.getLinks().add(expectedLink);
+		network.getLinks().add(secondLink);
+		SkynetAgent agent = new SkynetAgent(network.getNodeByIndex(2));
+		// act
+		Link nextLink = new Virus().findAndRemoveNextLink(network, agent);
+		// assert
+		assertEquals(expectedLink, nextLink);
+	}
+	@Test
+	public void should_find_getway_link_when_agent_near_getway_node() {
+		// arrange
+		Link firstLink = new Link(network.getNodeByIndex(0), network.getNodeByIndex(1));
+		Link secondLink = new Link(network.getNodeByIndex(1), network.getNodeByIndex(2));
+
+		network.getNodeByIndex(2).setGetway(true);
+		network.getLinks().add(firstLink);
+		network.getLinks().add(secondLink);
+		SkynetAgent agent = new SkynetAgent(network.getNodeByIndex(1));
+		// act
+		Link nextLink = new Virus().findAndRemoveNextLink(network, agent);
+		// assert
+		assertEquals(secondLink, nextLink);
+	}
+
+
 }
